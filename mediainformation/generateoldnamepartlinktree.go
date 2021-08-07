eaf147954193dd157395de7a36faacb05832e67d
Olaf Radicke <briefkasten@olaf-radicke.de>
2021-08-07 09:30:27 +0200

    Fix spell

--- mediainformation/generateoldnamepartlinktree.go
+++ mediainformation/generateoldnamepartlinktree.go
@@ -13,9 +13,9 @@ func (mediaInfo *MediaInformation) GenerateOldNamePartLinkTree() {
 		cl.InfoLogger.Println("++++++++++++++++++++++++ create old name category ++++++++++++++++++++")
 		cl.InfoLogger.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-		mediaInfo.SetAbsoluteContentLinkDirPath("00-olad-name-part/" + listOfParts[i])
-		mediaInfo.SetAbsoluteManifestLinkDirPath("00-olad-name-part/" + listOfParts[i])
-		mediaInfo.CreateLinkDirSubDir("00-olad-name-part/" + listOfParts[i])
+		mediaInfo.SetAbsoluteContentLinkDirPath("00-old-name-parts/" + listOfParts[i])
+		mediaInfo.SetAbsoluteManifestLinkDirPath("00-old-name-parts/" + listOfParts[i])
+		mediaInfo.CreateLinkDirSubDir("00-old-name-parts/" + listOfParts[i])
 		mediaInfo.CreateContentLink()
 		mediaInfo.CreateNewEmptyManifestFile()
 		mediaInfo.CreateManifestLink()
