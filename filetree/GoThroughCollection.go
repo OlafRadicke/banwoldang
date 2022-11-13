8612318578e1e7df682b117645f049a081d5c269
Olaf Radicke <briefkasten@olaf-radicke.de>
2022-11-13 15:08:17 +0100

    WIP

--- filetree/GoThroughCollection.go
+++ filetree/GoThroughCollection.go
@@ -1,15 +1,15 @@
 package filetree
 
-import (
-	"path/filepath"
+// import (
+// 	"path/filepath"
 
-	cl "github.com/OlafRadicke/banwoldang/customlogger"
-)
+// 	cl "github.com/OlafRadicke/banwoldang/customlogger"
+// )
 
-// GoThroughCollection This function gos through the source directory recursively.
-func (fileTree *FileTree) GoThroughCollection() {
-	err := filepath.Walk(fileTree.SourcePath, fileTree.fileHandler)
-	if err != nil {
-		cl.ErrorLogger.Println(err)
-	}
-}
+// // GoThroughCollection This function gos through the source directory recursively.
+// func (fileTree *FileTree) GoThroughCollection() {
+// 	err := filepath.Walk(fileTree.SourcePath, fileTree.fileHandler)
+// 	if err != nil {
+// 		cl.ErrorLogger.Println(err)
+// 	}
+// }
--- filetree/fileHandler.go
+++ filetree/fileHandler.go
@@ -1,51 +1,51 @@
 package filetree
 
-import (
-	"os"
-	"path/filepath"
+// import (
+// 	"os"
+// 	"path/filepath"
 
-	cl "github.com/OlafRadicke/banwoldang/customlogger"
-	"github.com/OlafRadicke/banwoldang/mediainformation"
-)
+// 	cl "github.com/OlafRadicke/banwoldang/customlogger"
+// 	"github.com/OlafRadicke/banwoldang/mediainformation"
+// )
 
-// fileHandler This function handel and prosessing the file operations.
-func (fileTree *FileTree) fileHandler(searchPath string, info os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
+// // fileHandler This function handel and prosessing the file operations.
+// func (fileTree *FileTree) fileHandler(searchPath string, info os.FileInfo, err error) error {
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if info.IsDir() {
-		// cl.InfoLogger.Println("Just a directory")
-		return nil
-	} else {
-		fileTree.Statistic.FoundedFiles++
+// 	if info.IsDir() {
+// 		// cl.InfoLogger.Println("Just a directory")
+// 		return nil
+// 	} else {
+// 		fileTree.Statistic.FoundedFiles++
 
-		if filepath.Ext(info.Name()) == ".xml" {
-			var err error
-			mediaInfo := mediainformation.NewMediaInformationByManifest(fileTree.progConfig, fileTree.Statistic, searchPath)
-			mediaInfo.SetAbsoluteManifestSourcePath(searchPath)
-			err = mediaInfo.ReconstructContenSourceFile()
-			if err != nil {
-				cl.ErrorLogger.Println("This manifest file has no media file: ", mediaInfo.Comments.FilePath)
-				cl.OrphanLogger.Println(mediaInfo.Comments.FilePath)
-				return nil
-			}
-			mediaInfo.ReadingManifestFile()
-			err = mediaInfo.CreateMediaFileHash()
-			if err != nil {
-				cl.ErrorLogger.Println("This manifest file has no media file: ", mediaInfo.Comments.FilePath)
-				return nil
-			}
-			mediaInfo.GenerateLinkDirTree()
+// 		if filepath.Ext(info.Name()) == ".xml" {
+// 			var err error
+// 			mediaInfo := mediainformation.NewMediaInformationByManifest(fileTree.progConfig, fileTree.Statistic, searchPath)
+// 			mediaInfo.SetAbsoluteManifestSourcePath(searchPath)
+// 			err = mediaInfo.ReconstructContenSourceFile()
+// 			if err != nil {
+// 				cl.ErrorLogger.Println("This manifest file has no media file: ", mediaInfo.Comments.FilePath)
+// 				cl.OrphanLogger.Println(mediaInfo.Comments.FilePath)
+// 				return nil
+// 			}
+// 			mediaInfo.ReadingManifestFile()
+// 			err = mediaInfo.CreateMediaFileHash()
+// 			if err != nil {
+// 				cl.ErrorLogger.Println("This manifest file has no media file: ", mediaInfo.Comments.FilePath)
+// 				return nil
+// 			}
+// 			mediaInfo.GenerateLinkDirTree()
 
-		} else {
-			mediaInfo := mediainformation.NewMediaInformation(fileTree.progConfig, fileTree.Statistic, searchPath)
-			mediaInfo.SetAbsoluteContentSourcePath(searchPath)
-			mediaInfo.ReconstructManifestFile()
-			// mediaInfo.CreateMediaFileHash()
-			mediaInfo.GenerateLinkDirTreeWithoutManifests()
+// 		} else {
+// 			mediaInfo := mediainformation.NewMediaInformation(fileTree.progConfig, fileTree.Statistic, searchPath)
+// 			mediaInfo.SetAbsoluteContentSourcePath(searchPath)
+// 			mediaInfo.ReconstructManifestFile()
+// 			// mediaInfo.CreateMediaFileHash()
+// 			mediaInfo.GenerateLinkDirTreeWithoutManifests()
 
-		}
-		return nil
-	}
-}
+// 		}
+// 		return nil
+// 	}
+// }
